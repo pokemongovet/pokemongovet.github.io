2c2122ec8c2e5a614c2eea9bfad1715e6a56503d
Evandro Santos <evan.its.me@gmail.com>
2016-08-04 00:27:41 -0300

    adicionando querystring

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
 /* original post: http://www.red-team-design.com/css3-pricing-table */
+
+$(document).on('ready',function(){
+	var choosenPackage = window.location.search.split('=')[1];
+
+	if(typeof choosenPackage !== 'undefined'){
+		$('.hide-basic').hide();
+
+		if(choosenPackage == 'profissional') {
+			$('.show-profissional').show();
+		}
+
+		if(choosenPackage == 'bussiness') {
+			$('.show-bussiness').show();
+		}
+
+		if(choosenPackage == 'promocional') {
+			$('.show-promocional').show();
+		}
+	}
+
+});
